15e70215ada27d4ab8b1ba0219269e91f008ff73
Shahnoor Khalidi <shahnoor.khalidi@inloop.com.au>
2024-03-19 22:31:05 +1100

    adding

--- obj/Debug/net8.0/Report-Razor-IronPDF.AssemblyInfo.cs
+++ obj/Debug/net8.0/Report-Razor-IronPDF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report-Razor-IronPDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0140db26c04675d5bf7da8d42acd71aa8d58486d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report-Razor-IronPDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report-Razor-IronPDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
